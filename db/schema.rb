213d0ebc9ffb9f682973dc1e5e926b8f324a3bf3
dwiyan <rahmanianto@gmail.com>
2016-03-14 17:38:06 +0700

    Automatically generated schema from rake

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310125951) do
+ActiveRecord::Schema.define(version: 20160313194245) do
+
+  create_table "agreement_channels", force: :cascade do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "agreement_id"
+    t.integer  "channel_type_id"
+    t.string   "customprice"
+  end
+
+  create_table "agreements", force: :cascade do |t|
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "PKSNumber",    null: false
+    t.string   "ApproverName"
+    t.string   "ApproverRole"
+    t.integer  "merchant_id"
+  end
+
+  create_table "channel_types", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "category"
+    t.string   "name"
+    t.string   "partner"
+    t.string   "charge"
+  end
+
+  create_table "merchant_customer_supports", force: :cascade do |t|
+    t.string  "email",       null: false
+    t.string  "telephone"
+    t.string  "emergency"
+    t.integer "merchant_id"
+  end
+
+  create_table "merchant_operationals", force: :cascade do |t|
+    t.string  "email",       null: false
+    t.string  "telephone"
+    t.string  "emergency"
+    t.integer "merchant_id"
+  end
+
+  create_table "merchant_owners", force: :cascade do |t|
+    t.string  "email",       null: false
+    t.string  "name"
+    t.string  "address"
+    t.string  "telephone"
+    t.integer "merchant_id"
+  end
+
+  create_table "merchant_pics", force: :cascade do |t|
+    t.string  "email",       null: false
+    t.string  "name"
+    t.string  "address"
+    t.string  "telephone"
+    t.integer "merchant_id"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.integer  "userid",                 null: false
     t.string   "name"
     t.string   "website"
     t.string   "email"
@@ -29,8 +84,16 @@ ActiveRecord::Schema.define(version: 20160310125951) do
     t.integer  "ownershiptype"
     t.decimal  "revenueseachmonth"
     t.decimal  "creditcardpaymentratio"
+    t.string   "registrationlink"
+    t.integer  "user_id"
   end
 
-  add_index "merchants", ["userid"], name: "index_merchants_on_userid", unique: true
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "email",      null: false
+    t.string   "pass"
+    t.string   "role"
+  end
 
 end
