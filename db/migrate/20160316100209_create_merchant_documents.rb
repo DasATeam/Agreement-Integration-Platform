b24369e17daa2994f07fb5e748d4db720461dc67
dwiyan <rahmanianto@gmail.com>
2016-03-17 20:16:41 +0700

    Updating database and model for document

--- db/migrate/20160316100209_create_merchant_documents.rb
+++ db/migrate/20160316100209_create_merchant_documents.rb
@@ -1,8 +1,11 @@
 class CreateMerchantDocuments < ActiveRecord::Migration
   def change
     create_table :merchant_documents do |t|
-
       t.timestamps null: false
+      t.references(:merchant, foreign_key: true, index: true)
+      t.references(:agreement, foreign_key: true, index: true)
+      t.references(:document_type, foreign_key: true)
+      t.string(:path)
     end
   end
 end
