76b21c12184a6788b115c8a57a546c23db799392
fatia kusuma <fatia.dewi@gmail.com>
2016-03-18 14:29:24 +0700

    deleted reference and foreign key in bank account migration

--- db/migrate/20160311073137_add_merchant_to_bank_account.rb
+++
@@ -1,6 +0,0 @@
-class AddMerchantToBankAccount < ActiveRecord::Migration
-  def change
-    add_reference :bank_accounts, :merchant, index: true, foreign_key: true
-    add_foreign_key :bank_accounts, :merchant
-  end
-end
