5d257a34dd6705a25501fc333858bb0565d28ffe
Wiratmika <wiratmika@gmail.com>
2016-03-27 18:18:32 +0700

    Add the working "agree" method which will update the agreement's has_agree to 1 and then redirect to merchant's dashboard

--- app/controllers/term_controller.rb
+++ app/controllers/term_controller.rb
@@ -6,5 +6,12 @@ class TermController < ApplicationController
   end
 
   def agree
+    @merchant = User.find(session[:user_id]).merchant
+    @agreement = @merchant.agreements.first
+    @agreement.has_agree = true
+    @agreement.save
+
+    redirect_to controller: "user", action: "merchant_new", hash: @merchant.registrationlink
   end
 end
+
