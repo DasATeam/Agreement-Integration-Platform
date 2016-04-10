86f744050ef0939da42736eafd46c4c88c9b428a
dwiyan <rahmanianto@gmail.com>
2016-04-11 06:35:07 +0700

    Creating Agreement controller testing

---
+++ spec/controllers/agreement_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe AgreementController, type: :controller do
+  describe "GET new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do
+    it "create new @user and @merchant" do
+      name = "anyemail@AnyShop.com"
+      email = "Any Name"
+      website = "AnyShop.com"
+
+      user = User.create(email: email)
+      user.role = "merchant"
+      user.id = user.id + 1
+      # merchant = Merchant.create(name: name, website: website)
+
+      post :create, {form: { email: email, name: name, website: website }}
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it "responds to html by default" do
+      name = "anyemail@AnyShop.com"
+      email = "Any Name"
+      website = "AnyShop.com"
+
+      post :create, {form: { email: email, name: name, website: website }}
+      expect(response.content_type).to eq "text/html"
+    end
+  end
+end
