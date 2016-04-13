7b42cef4590ea0c1237247efb2399a93ca05850a
fatia kusuma <fatia.dewi@gmail.com>
2016-04-13 11:10:24 +0700

    Create model and migration

--- app/models/document_type.rb
+++ app/models/document_type.rb
@@ -1,3 +1,4 @@
 class DocumentType < ActiveRecord::Base
 	has_and_belongs_to_many(:channels, class_name: 'ChannelType', join_table: 'channel_types_document_types')
+  has_one(:document_links)
 end
---
+++ spec/models/document_link_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DocumentLink, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
