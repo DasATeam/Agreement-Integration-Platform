6bc8250ff31cde747d6d2fa6cbf3f9db7490c68d
dwiyan <rahmanianto@gmail.com>
2016-04-13 12:22:28 +0700

    Creating set password feature for merchant
    merchant should set their password the first time they login to apps

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,22 @@
 class UserController < ApplicationController
-	def merchant_new
-		merchant = Merchant.find_by(registration_link: params[:hash])
-		user = merchant.user
-		session[:user_id] = user.id
-		render "test"
-	end
+  def merchant_set_password
+    merchant = Merchant.find_by(registration_link: params[:hash])
+    user = merchant.user
+    if(params[:pass])
+      user.pass = params[:pass][:password]
+      if(params[:pass][:password] != params[:pass][:password_confirmation])
+        flash[:error] = "Kedua password tidak sama";
+      elsif(params[:pass][:password].length < 6 || params[:pass][:password].length > 20)
+        flash[:error] = 'password harus memiliki 6 - 20 karakter'
+      else
+        user.set_password(params[:pass][:password])
+        merchant.registration_link = ""
+        merchant.save
+
+        redirect_to(controller: "merchantform", action: "edit")
+      end
+    end
+
+  end
+
 end
