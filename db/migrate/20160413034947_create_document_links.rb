079aa9c91435b78f9a9ff089329333a1cf6a96cd
fatia kusuma <fatia.dewi@gmail.com>
2016-04-14 20:06:25 +0700

    modified document_links migration

--- db/migrate/20160413034947_create_document_links.rb
+++ db/migrate/20160413034947_create_document_links.rb
@@ -2,7 +2,7 @@ class CreateDocumentLinks < ActiveRecord::Migration
   def change
     create_table :document_links do |t|
       t.timestamps null: false
-      t.references(:document_types, foreign_key: true)
+      t.references(:document_type, foreign_key: true)
       t.string :link
     end
 
