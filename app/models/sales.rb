bd8d5b6a51ff15ccebfc72c5a6bce8bc4ac557b8
Rafika <rafika1209@gmail.com>
2016-04-17 20:45:20 +0700

    create controller for sales

--- app/models/sales.rb
+++ app/models/sales.rb
@@ -1,6 +1,6 @@
 class Sales < ActiveRecord::Base
   self.table_name = "sales"
   belongs_to(:user)
-  has_and_belongs_to_many(:merchants, class_name: 'Merchant', join_table: 'sales_agreements')
-
+  has_many(:sales_merchants)
+  has_many(:merchants, through: :sales_merchants)
 end
