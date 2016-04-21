0590f5d24e689f0717297abf8ec9d4d84168da4c
dwiyan <rahmanianto@gmail.com>
2016-04-21 15:38:52 +0700

    db reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328015922) do
+ActiveRecord::Schema.define(version: 20160327235810) do
 
   create_table "agreement_channels", force: :cascade do |t|
     t.datetime "created_at",      null: false
