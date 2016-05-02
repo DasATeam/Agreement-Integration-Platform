ef3e3cce76074efef84ced9b0175232fd40e8a5d
Wiratmika <wiratmika@gmail.com>
2016-05-02 07:40:01 +0700

    Fix migration and testing errors

--- db/migrate/20160413034421_create_document_paths.rb
+++ db/migrate/20160413034421_create_document_paths.rb
@@ -2,7 +2,7 @@ class CreateDocumentPaths < ActiveRecord::Migration
   def change
     create_table :document_paths do |t|
       t.timestamps null: false
-      t.references(:merchant_documents, foreign_key: true, index: true)
+      # t.references(:merchant_documents, foreign_key: true, index: true)
       t.string :path
     end
   end
