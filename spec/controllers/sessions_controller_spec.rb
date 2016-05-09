b01031761e41c75a000297089b36023facce7354
Wiratmika <wiratmika@gmail.com>
2016-05-09 08:22:49 +0700

    Add test for creating new session

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "create" do
+    it "should log the user in when they enter the correct email and password" do
+      fill_in 'email', :with => 'john@veritrans.com'
+      fill_in 'password', :with => 'ppl'
 
+      click_button 'Log in'
+
+      expect(session[:user_id]).not_to be_nil
+    end
+  end
 end
