8f54a974c822be5ae8d613448d6b80ade7baf332
Wiratmika <wiratmika@gmail.com>
2016-05-09 09:25:40 +0700

    Add testing for logout feature

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,24 +6,20 @@ RSpec.describe SessionsController, type: :controller do
     before :all do
       @user = User.create(email: 'john@veritrans.com', role: 'sales')
       @user.set_password('ppl')
-      pp @user
     end
 
     it 'should log the user in when they enter the correct email and password' do
       post(:create, session: {email: 'john@veritrans.com', password: 'ppl'})
 
-      # expect(session[:user_id]).not_to be_nil
-      pp flash[:notice]
+      expect(session[:user_id]).not_to be_nil
     end
 
-    # it 'should log the user out after they are logged in' do
-    #   post(:create, session: {email: 'john@veritrans.com', password: 'ppl'})
+    it 'should log the user out after they are logged in' do
+      post(:create, session: {email: 'john@veritrans.com', password: 'ppl'})
+      expect(session[:user_id]).not_to be_nil
 
-    #   expect(session[:user_id]).not_to be_nil
-
-    #   post(:delete)
-
-    #   expect(session[:user_id]).should be_nil
-    # end
+      get(:destroy)
+      expect(session[:user_id]).to be_nil
+    end
   end
 end
