fd3f8b41500d013d9a43e8a38bb873b1d2849cfe
dwiyanr <rahmanianto@gmail.com>
2016-05-16 03:20:00 +0700

    Fixing Agreement bug

--- app/controllers/term_controller.rb
+++ app/controllers/term_controller.rb
@@ -11,15 +11,17 @@ class TermController < ApplicationController
   def doc
     @user = current_user
     @merchant = @user.merchant
-    @agreement = @merchant.first
+    @agreement = @merchant.agreements.first
     @term = Point.first
 
 
     respond_to do |format|
       format.html do
-        render :pdf => "Agreement",
-        :disposition => "attachment",
-        :template => "term/doc.html.erb"
+        render pdf: "Agreement",
+        disposition: "inline",
+        template: "term/doc.html.erb",
+        page_size: "A4",
+        margin: {top: 20, left: 20, bottom: 20, right: 20}
       end
     end
   end
