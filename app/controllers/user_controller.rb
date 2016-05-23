604eca772c6831d2a6d9df4b7faaf5f2006da275
Wiratmika <wiratmika@gmail.com>
2016-05-23 07:13:24 +0700

    Finalize the bugfix on set password using TDD

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,21 @@
 class UserController < ApplicationController
   def merchant_set_password
-    merchant = Merchant.find_by(registration_link: params[:hash])
-    user = merchant.user
+    @merchant = Merchant.find_by(registration_link: params[:hash])
+    @user = User.find(@merchant.user_id)
+
     if(params[:pass])
-      user.pass = params[:pass][:password]
       if(params[:pass][:password] != params[:pass][:password_confirmation])
-        flash[:error] = "Kedua password tidak sama";
+        flash[:error] = 'Kedua password tidak sama';
       elsif(params[:pass][:password].length < 8)
         flash[:error] = 'password harus memiliki 8 karakter atau lebih'
       else
-        user.set_password(params[:pass][:password])
-        merchant.registration_link = ""
-        merchant.save
+        @user.set_password(params[:pass][:password])
+        @user.save
+        @merchant.registration_link = ''
+        @merchant.save
 
-        redirect_to(controller: "merchantform", action: "edit")
+        redirect_to(controller: 'merchantform', action: 'edit')
       end
     end
-
   end
-
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,43 +1,47 @@
 require 'rails_helper'
 require 'digest/md5'
+require 'pp'
 
 RSpec.describe UserController, type: :controller do
+  before :all do
+    @user = User.create(id: 8, email: 'test@email.com', role: 'sales')
+    @merchant = Merchant.create(name: 'test', website: 'test')
+    @link = Digest::MD5.hexdigest(@user.email)
+    @merchant.registration_link = @link
+    @merchant.user = @user
+    @merchant.save
+  end
 
-  context "User set password" do
-    before :all do
-      @user = User.create(email: "test@email.com")
-      @merchant = Merchant.create(name: "test", website: "test")
-      @link = Digest::MD5.hexdigest(@user.email)
-      @merchant.registration_link = @link
-      @merchant.user = @user
-    end
+  render_views
 
-    it "Call set_password function" do
-      mock_pass = "123456"
+  it 'should render the set password page if the merchant had never set the password' do
+    get :merchant_set_password, { hash: @link }
+    expect(response).to render_template('merchant_set_password')
+  end
 
-      post(:merchant_set_password, link: @link, pass: {password: mock_pass, password_confirmation: mock_pass})
-      expect(@user).to receive(:set_password).with(mock_pass)
-    end
+  it 'should prevent the user to enter password that is too short' do
+    mock_pass = '123456'
 
-    it "Give error when password too short" do
-      mock_pass = "12345"
+    post(:merchant_set_password, hash: @link, pass: {password: mock_pass, password_confirmation: mock_pass})
+    expect(flash[:error]).not_to be_nil
+  end
 
-      post(:merchant_set_password, link: @link, pass: {password: mock_pass, password_confirmation: mock_pass})
-      expect(flash[:error]).not_to be_nil
-    end
+  it 'should prevent the user when password is different with its confirmation' do
+    mock_pass = '12345678'
+    diff_pass = '87654321'
 
-    it "Give error when password different with its confirmation" do
-      mock_pass = "123456"
-      diff_pass = "654321"
+    post(:merchant_set_password, hash: @link, pass: {password: mock_pass, password_confirmation: diff_pass})
+    expect(flash[:error]).not_to be_nil
+  end
 
-      post(:merchant_set_password, link: @link, pass: {password: mock_pass, password_confirmation: diff_pass})
-      expect(flash[:error]).not_to be_nil
-    end
+  it 'should save the merchant password if the password is valid' do
+    mock_pass = '12345678'
 
-    # it "renders the new template" do
-    #   get (:merchant_set_password, link: @link
-    #   expect(response).to render_template("merchant_set_password")
-    # end
+    post(:merchant_set_password, hash: @link, pass: {password: mock_pass, password_confirmation: mock_pass})
 
+    @user.set_password '12345678'
+
+    expect(flash[:error]).to be_nil
+    expect @user.password_match? '12345678'
   end
 end
